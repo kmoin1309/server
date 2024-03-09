a1eabf53b55a798924355748f9e0ae004e42575c
kmoin1309 <128276854+kmoin1309@users.noreply.github.com>
2024-03-10 01:06:36 +0530

    index.js added

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const paymentRoutes = require("./routes/Payment");
+const courseRoutes = require("./routes/Course");
+
+const database = require("./config/database");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { cloudinaryConnect } = require("./config/cloudinary");
+const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
+
+const PORT = process.env.PORT || 4000;
+
+//database connect
+database.connect();
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+  })
+);
+//cloudinary connecction
+cloudinaryConnect();
+
+// /routes
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/profile", profileRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/payment", paymentRoutes);
+
+//def route
+app.get("/", (req, res) => {
+  return res.json({
+    success: true,
+    message: "Your Server is up and running...",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running at ${PORT}`);
+});
